0f9f3b01723714bc72d6eeac2fd134b94643eca4
Pierre Leplay <pierre.leplay@gmail.com>
2023-11-28 17:38:55 +0100

    obsolescence

--- src/test/java/fr/softeam/cuillereapi/service/RestaurantServiceTest.java
+++ src/test/java/fr/softeam/cuillereapi/service/RestaurantServiceTest.java
@@ -59,29 +59,4 @@ class RestaurantServiceTest {
         assertEquals(10.5d,res.getPlats().get(0).getPrix());
     }
 
-
-    @Test
-    void getRestaurantFeteDesMeres2019(){
-        Restaurant leRipailleur= ModelHelper.createRestaurantLeRipailleur();
-
-        CategoriePlat cpPrincipal=new CategoriePlat();
-        cpPrincipal.setCode("CP");
-        cpPrincipal.setLibelle("Plat principal");
-
-        Plat boeuf=new Plat();
-        boeuf.setLibelle("Boeuf bourguignon");
-        boeuf.setCategoriePlat(cpPrincipal);
-        boeuf.setPrix(10.5d);
-        boeuf.setRestaurant(leRipailleur);
-
-        leRipailleur.setPlats(List.of(boeuf));
-
-        when(repository.getDetailsById(3l)).thenReturn(leRipailleur);
-
-        RestaurantDetailDto res=restaurantService.getRestaurantFeteDesMeres2019(3l);
-
-        assertEquals("Le Ripailleur",res.getNom());
-        assertEquals(1,res.getPlats().size());
-        assertEquals(9.45d,res.getPlats().get(0).getPrix(),0.01d);
-    }
 }
