41e5a67f2e8fc7bbbbaf8f2c61aaf62e7a2acfc1
Pierre Leplay <pierre.leplay@gmail.com>
2023-11-28 12:13:54 +0100

    Partie asynchrone

--- src/main/java/fr/softeam/cuillereapi/controler/AvisControler.java
+++ src/main/java/fr/softeam/cuillereapi/controler/AvisControler.java
@@ -6,8 +6,11 @@ import fr.softeam.cuillereapi.api.AvisDto;
 import fr.softeam.cuillereapi.repository.AvisRepository;
 import fr.softeam.cuillereapi.repository.RestaurantRepository;
 import fr.softeam.cuillereapi.service.AvisService;
+import fr.softeam.cuillereapi.service.DatabaseService;
 import fr.softeam.cuillereapi.service.KafkaAvisService;
 import io.micrometer.observation.annotation.Observed;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -25,6 +28,7 @@ import java.util.List;
 @CrossOrigin("*")
 public class AvisControler {
 
+	Logger logger = LoggerFactory.getLogger(AvisControler.class);
 	private AvisService avisService;
 	private final AvisRepository avisRepository;
 	private final RestaurantRepository restaurantRepository;
@@ -85,8 +89,7 @@ public class AvisControler {
 
 	@KafkaListener(topics = "cuillere-avis")
 	public void listenGroupFoo(AvisCreationDto message) {
-		//FIXME supprimer system.out
-		System.out.println("Received Message in group foo: " + message.getAuteur());
+		logger.info("Création de l'avis pour restaurant {}",message.getIdRestaurant());
 		avisService.creerAvis(message);
 	}
 	//FIXME test consumer
--- src/test/java/fr/softeam/cuillereapi/PerfTest.java
+++ src/test/java/fr/softeam/cuillereapi/PerfTest.java
@@ -8,42 +8,45 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
 public class PerfTest {
 	@Test
 	void test(){
-		callGetRestaurant();
+		callGetRestaurant(1);
 	}
 
 	//
 	@Test
 	void testParrallelGetRestaurant(){
-		IntStream.range(0, 1000_000).parallel().forEach(x->callGetRestaurant());
+		AtomicInteger cpt= new AtomicInteger();
+		IntStream.range(0, 10_000_000).parallel().forEach(x->callGetRestaurant(cpt.getAndIncrement()));
 	}
 
 
 	@Test
 	void testCreerAvis(){
-		callPostAvis();
+		callPostAvis(1);
 	}
 
 	@Test
 	void testParrallelCreerAvis(){
-		IntStream.range(0, 1000).parallel().forEach(x->callPostAvis());
+		AtomicInteger cpt= new AtomicInteger();
+		IntStream.range(0, 100).parallel().forEach(x->callPostAvis(cpt.getAndIncrement()));
 	}
 
-	private static void callGetRestaurant() {
+	private static void callGetRestaurant(int x) {
 		RestTemplate restTemplate = new RestTemplate();
-		String fooResourceUrl
-			= "http://localhost:8080/restaurants";
+		//FIXME à la place trouver une facon de récupérer dynmiquement l'id
+		String fooResourceUrl = "http://localhost:8080/restaurants/160956";
 		ResponseEntity<String> response
 			= restTemplate.getForEntity(fooResourceUrl , String.class);
-		System.out.println(" " + Thread.currentThread().getName());
+		System.out.println(x+" " + Thread.currentThread().getName());
 		Assertions.assertEquals(response.getStatusCode(), HttpStatus.OK);
 	}
 
-	private static void callPostAvis() {
+	private static void callPostAvis(int x) {
 		RestTemplate restTemplate = new RestTemplate();
 		String fooResourceUrl
 			= "http://localhost:8080/avis";
@@ -58,9 +61,10 @@ public class PerfTest {
 
 		HttpEntity<AvisCreationDto> request = new HttpEntity<>(avisCreation);
 
+		//FixME supprimer system.out
 		ResponseEntity<Long> response
 			= restTemplate.postForEntity(fooResourceUrl ,  request, Long.class);
-		System.out.println(" " + Thread.currentThread().getName());
+		System.out.println(x+" " + Thread.currentThread().getName());
 		Assertions.assertEquals(response.getStatusCode(), HttpStatus.CREATED);
 	}
 }
