bdb72bcdec54d628ee5278b3f48a5b3b7f9a2326
Pierre Leplay <pierre.leplay@gmail.com>
2023-11-28 12:23:18 +0100

    Partie asynchrone

--- src/test/java/fr/softeam/cuillereapi/PerfTest.java
+++ src/test/java/fr/softeam/cuillereapi/PerfTest.java
@@ -1,8 +1,11 @@
 package fr.softeam.cuillereapi;
 
 import fr.softeam.cuillereapi.api.AvisCreationDto;
+import fr.softeam.cuillereapi.controler.AvisControler;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
 public class PerfTest {
+	static Logger logger = LoggerFactory.getLogger(PerfTest.class);
+
 	@Test
 	void test(){
 		callGetRestaurant(1);
@@ -36,17 +41,17 @@ public class PerfTest {
 		IntStream.range(0, 100).parallel().forEach(x->callPostAvis(cpt.getAndIncrement()));
 	}
 
-	private static void callGetRestaurant(int x) {
+	private static void callGetRestaurant(int cpt) {
 		RestTemplate restTemplate = new RestTemplate();
 		//FIXME à la place trouver une facon de récupérer dynmiquement l'id
 		String fooResourceUrl = "http://localhost:8080/restaurants/160956";
 		ResponseEntity<String> response
 			= restTemplate.getForEntity(fooResourceUrl , String.class);
-		System.out.println(x+" " + Thread.currentThread().getName());
+		logger.info("cpt "+cpt);
 		Assertions.assertEquals(response.getStatusCode(), HttpStatus.OK);
 	}
 
-	private static void callPostAvis(int x) {
+	private static void callPostAvis(int cpt) {
 		RestTemplate restTemplate = new RestTemplate();
 		String fooResourceUrl
 			= "http://localhost:8080/avis";
@@ -61,10 +66,9 @@ public class PerfTest {
 
 		HttpEntity<AvisCreationDto> request = new HttpEntity<>(avisCreation);
 
-		//FixME supprimer system.out
 		ResponseEntity<Long> response
 			= restTemplate.postForEntity(fooResourceUrl ,  request, Long.class);
-		System.out.println(x+" " + Thread.currentThread().getName());
+		logger.info("cpt "+cpt);
 		Assertions.assertEquals(response.getStatusCode(), HttpStatus.CREATED);
 	}
 }
